f9dac80cb6cdbb78f63beb5fb397b0ec6330863e
MunsonZhang <978068205@qq.com>
2019-04-03 18:07:22 +0800

    开始为图片添加缓存

---
+++ app/src/main/java/com/example/munson/mydemo02/common/GirlCompress.java
@@ -0,0 +1,17 @@
+package com.example.munson.mydemo02.common;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+
+public class GirlCompress {
+
+    private static final String TAG = "ImageCompress";
+
+    public GirlCompress() {
+
+    }
+
+    public Bitmap decodeBitmapResource(Resources res, int resId, int reqWidth, int reqHeight){
+
+    }
+}
